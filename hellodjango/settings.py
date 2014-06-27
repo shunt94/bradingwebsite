039a35b98876ce0eedf16f173562467f73cafbbd
connor <conninie@gmail.com>
2014-06-27 21:00:34 +0100

    maybe work?

--- hellodjango/settings.py
+++ hellodjango/settings.py
@@ -76,8 +76,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = 'staticfiles'
 
-STATICFILES_DIRS= (
-    os.path.join(PROJECT_PATH, 'static')
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_PATH, 'static'),
 )
 
 import dj_database_url
