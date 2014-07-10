bd0fd2268b4491bc166a3c5e5b328773a847bced
connor <conninie@gmail.com>
2014-07-10 17:00:15 +0100

    connors changes

--- hellodjango/apps/brading/models.py
+++ hellodjango/apps/brading/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class Bookmark (models.Model):
     url = models.CharField(max_length=255)
     name = models.CharField(max_length=255)
+
+    def __unicode__(self):
+        return "%s" % self.name
